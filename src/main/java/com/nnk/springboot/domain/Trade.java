0c319acc637b15b85a3af9c036dbd9f1b35e1fb1
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-09-30 10:00:09 +0200

    [FEAT] TRADE ENTITY - Update based on SQL SCHEMA & REQUIREMENTS PROVIDED to support further next DATA JPA implementation

--- src/main/java/com/nnk/springboot/domain/Trade.java
+++ src/main/java/com/nnk/springboot/domain/Trade.java
@@ -1,12 +1,115 @@
 package com.nnk.springboot.domain;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Entity
 @Table(name = "trade")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Trade {
-    // TODO: Map columns in data table TRADE with corresponding java fields
+
+	private static final int VARIABLE_LENGTH_10 = 10;
+
+	private static final int VARIABLE_LENGTH_30 = 30;
+
+	private static final int VARIABLE_LENGTH_125 = 125;
+
+	@Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "trade_id", unique = true, nullable = false)
+    private Integer tradeId;
+
+    @Column(name = "account", length = VARIABLE_LENGTH_30, nullable = false)
+    private String account;
+
+    @Column(name = "type", length = VARIABLE_LENGTH_30, nullable = false)
+    private String type;
+
+    @Column(name = "buy_quantity", columnDefinition="Decimal(10,2) default '0.00'")
+    private Double buyQuantity;
+
+    @Column(name = "sell_quantity", columnDefinition="Decimal(10,2) default '0.00'")
+    private Double sellQuantity;
+
+    @Column(name = "buy_price", columnDefinition="Decimal(10,2) default '0.00'")
+    private Double buyPrice;
+
+    @Column(name = "sell_price", columnDefinition="Decimal(10,2) default '0.00'")
+    private Double sellPrice;
+
+//    @Column(name = "trade_date", nullable = false, updatable = false)
+//    @CreationTimestamp
+//    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "trade_date")
+    private LocalDateTime tradeDate;
+
+    @Column(name = "security", length = VARIABLE_LENGTH_125, nullable = true)
+    private String security;
+
+    @Column(name = "status", length = VARIABLE_LENGTH_10)
+    private String status;
+
+    @Column(name = "trader", length = VARIABLE_LENGTH_125)
+    private String trader;
+
+    @Column(name = "benchmark", length = VARIABLE_LENGTH_125)
+    private String benchmark;
+
+    @Column(name = "book", length = VARIABLE_LENGTH_125)
+    private String book;
+
+    @Column(name = "creation_name", length = VARIABLE_LENGTH_125)
+    private String creationName;
+
+//    @CreationTimestamp
+//    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @Column(name = "revision_name", length = VARIABLE_LENGTH_125)
+    private String revisionName;
+
+//    @UpdateTimestamp
+//    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "revision_date")
+    private LocalDateTime revisionDate;
+
+    @Column(name = "deal_name", length = VARIABLE_LENGTH_125)
+    private String dealName;
+
+    @Column(name = "deal_type", length = VARIABLE_LENGTH_125)
+    private String dealType;
+
+    @Column(name = "source_list_id", length = VARIABLE_LENGTH_125)
+    private String sourceListId;
+
+    @Column(name = "side", length = VARIABLE_LENGTH_125)
+    private String side;
+
+    public Trade(String account, String type) {
+        this.account = account;
+        this.type = type;
+    }
+
+    public Trade(final String account, final String type, final double buyQuantity) {
+        this.account = account;
+        this.type = type;
+        this.buyQuantity = buyQuantity;
+    }
+    
 }
