c3a74defb8d6db69e7aa471b0468d35fd58015dc
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-09-30 10:26:25 +0200

    [FEAT] BIDLIST ENTITY - Update based on SQL SCHEMA & REQUIREMENTS PROVIDED to support further next DATA JPA implementation

--- src/main/java/com/nnk/springboot/domain/BidList.java
+++ src/main/java/com/nnk/springboot/domain/BidList.java
@@ -1,15 +1,99 @@
 package com.nnk.springboot.domain;
 
-import org.springframework.beans.factory.annotation.Required;
+import java.time.LocalDateTime;
 
-import javax.persistence.*;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotBlank;
-import java.sql.Date;
-import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "bidlist")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BidList {
-    // TODO: Map columns in data table BIDLIST with corresponding java fields
+
+	private static final int VARIABLE_LENGTH_30 = 30;
+
+	private static final int VARIABLE_LENGTH_125 = 125;
+	
+	@Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "bid_list_id")
+    private Integer bidListId;
+
+	@Column(name = "account", length = VARIABLE_LENGTH_30, nullable = false)
+    private String account;
+
+    @Column(name = "type", length = VARIABLE_LENGTH_30, nullable = false)
+	private String type;
+
+    @Column(name = "bid_quantity", columnDefinition="Decimal(10,2) default '0.00'")
+    private Double bidQuantity;
+
+    @Column(name = "ask_quantity", columnDefinition="Decimal(10,2) default '0.00'")
+    private Double askQuantity;
+
+    @Column(name = "bid", columnDefinition="Decimal(10,2) default '0.00'")
+    private Double bid;
+
+    @Column(name = "ask", columnDefinition="Decimal(10,2) default '0.00'")
+    private Double ask;
+
+    @Column(name = "benchmark", length = VARIABLE_LENGTH_125)
+    private String benchmark;
+
+//    @CreationTimestamp
+//    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "bid_list_date")
+    private LocalDateTime bidListDate;
+
+    private String commentary;
+
+    private String security;
+
+    private String status;
+
+    private String trader;
+
+    private String book;
+
+    @Column(name = "creation_name")
+    private String creationName;
+
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    @Column(name = "revision_name")
+    private String revisionName;
+
+    @Column(name = "revision_date")
+    private LocalDateTime revisionDate;
+
+    @Column(name = "deal_name")
+    private String dealName;
+
+    @Column(name = "deal_type")
+    private String dealType;
+
+    @Column(name = "source_list_id")
+    private String sourceListId;
+
+    private String side;
+
+    public BidList(final String account, final String type, final Double bidQuantity) {
+        this.account = account;
+        this.type = type;
+        this.bidQuantity = bidQuantity;
+    }
+    
 }
