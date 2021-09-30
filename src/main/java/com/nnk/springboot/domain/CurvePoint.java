5c8a8c9b4df2e8f96397ba06ea629781ee9247b2
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-09-30 10:24:03 +0200

    [FEAT] CURVEPOINT ENTITY - Update based on SQL SCHEMA & REQUIREMENTS PROVIDED to support further next DATA JPA implementation

--- src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -1,15 +1,56 @@
 package com.nnk.springboot.domain;
 
-import org.hibernate.validator.constraints.Length;
+import java.time.LocalDateTime;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Entity
 @Table(name = "curvepoint")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CurvePoint {
-    // TODO: Map columns in data table CURVEPOINT with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Integer id;
+
+    @Column(name = "curve_id")
+    private Integer curveId;
+
+//    @CreationTimestamp
+//    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "as_of_date")
+    private LocalDateTime asOfDate;
+
+    @Column(name = "term", columnDefinition="Decimal(10,2) default '0.00'")
+    private Double term;
+
+    @Column(name = "value", columnDefinition="Decimal(10,2) default '0.00'")
+    private Double value;
+
+//    @CreationTimestamp
+//    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "creation_date")
+    private LocalDateTime creationDate;
+
+    public CurvePoint(final Integer curveId, final Double term, final Double value) {
+        this.curveId = curveId;
+        this.term = term;
+        this.value = value;
+    }	
+	
 }
