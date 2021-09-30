c40652787f995057a87502f628ddcd8a7e5d3b1d
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-09-30 09:55:32 +0200

    [FEAT] USER ENTITY - Update based on SQL SCHEMA & REQUIREMENTS PROVIDED to support further next DATA JPA implementation

--- src/main/java/com/nnk/springboot/domain/User.java
+++ src/main/java/com/nnk/springboot/domain/User.java
@@ -1,60 +1,94 @@
 package com.nnk.springboot.domain;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotBlank;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = "username")})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-    @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+
+	private static final int VARIABLE_LENGTH_125 = 125;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
     private Integer id;
-    @NotBlank(message = "Username is mandatory")
+
+	@Column(name = "username", length = VARIABLE_LENGTH_125, nullable = false)
+	@NotBlank(message = "Username is mandatory")
     private String username;
-    @NotBlank(message = "Password is mandatory")
+
+	@Column(name = "password", length = VARIABLE_LENGTH_125, nullable = false)
+	@NotBlank(message = "Password is mandatory")
     private String password;
-    @NotBlank(message = "FullName is mandatory")
+
+	@Column(name = "fullname", length = VARIABLE_LENGTH_125, nullable = false)
+	@NotBlank(message = "FullName is mandatory")
     private String fullname;
-    @NotBlank(message = "Role is mandatory")
+
+	@Column(name = "role", length = VARIABLE_LENGTH_125, nullable = false)
+	@NotBlank(message = "Role is mandatory")
     private String role;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
+    public User(final String username, final String password, final String fullname, final String role) {
         this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getFullname() {
-        return fullname;
-    }
-
-    public void setFullname(String fullname) {
         this.fullname = fullname;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
         this.role = role;
     }
+
+//    public Integer getId() {
+//        return id;
+//    }
+//
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getFullname() {
+//        return fullname;
+//    }
+//
+//    public void setFullname(String fullname) {
+//        this.fullname = fullname;
+//    }
+//
+//    public String getRole() {
+//        return role;
+//    }
+//
+//    public void setRole(String role) {
+//        this.role = role;
+//    }
 }
