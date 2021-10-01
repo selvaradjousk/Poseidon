c8a91da666bd8dc4231ab972695bf771bcbf45ba
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-01 11:06:23 +0200

    [UNIT TESTS] SERVICE - UPDATE USER Test Impl <EXEC ORDER> => RUN FAILED

---
+++ src/test/java/com/nnk/springboot/UnitTests/service/UserServiceUpdateTest.java
@@ -0,0 +1,166 @@
+package com.nnk.springboot.UnitTests.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nnk.springboot.domain.User;
+import com.nnk.springboot.dto.UserDTO;
+import com.nnk.springboot.repository.UserRepository;
+import com.nnk.springboot.service.UserService;
+import com.nnk.springboot.util.UserMapper;
+
+@DisplayName("User Service Update User- UNIT TESTS")
+@ExtendWith(MockitoExtension.class)
+class UserServiceUpdateTest {
+
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+    
+    @Mock
+    private UserMapper userMapper;
+    
+    @Mock
+    private BCryptPasswordEncoder passwordEncoder;
+	
+    @Mock
+    private ObjectMapper objectMapper;
+	
+    private static UserDTO testUserDTO1, testUserDTO2, userUpdatedDTO;
+    
+    private static User testUser1, testUser2, userUpdated;
+    
+    
+    @BeforeEach
+    public void setUp() {
+        objectMapper = new ObjectMapper();
+        testUserDTO1 = UserDTO.builder()
+        		.username("Username")
+        		.password("Password&1")
+        		.fullname("Fullname")
+        		.role("USER")
+        		.build();
+        
+        testUserDTO2 = UserDTO.builder()
+        		.username("Username")
+        		.password("Password&1")
+        		.fullname("Fullname")
+        		.role("USER")
+        		.build();
+        
+
+        
+        testUser1 = User.builder()
+        		.username("Username")
+        		.password("Password&1")
+        		.fullname("Fullname")
+        		.role("USER")
+        		.build();
+        
+        testUser2 = User.builder()
+        		.username("Username")
+        		.password("Password&1")
+        		.fullname("Fullname")
+        		.role("USER")
+        		.build();
+        
+       
+    }
+  
+    // ***********************************************************************************
+
+    @DisplayName("Test UPDATE New USER")
+    @Nested
+    class TestUpdateNewUser {  
+    	
+        @BeforeEach
+        public void init() {
+        	
+ 	       userUpdated = User.builder()
+	        		.id(1)
+	        		.username("Username")
+	        		.password("Password&1")
+	        		.fullname("FullnameUpdate")
+	        		.role("USER")
+	        		.build();
+	        
+	        userUpdatedDTO = UserDTO.builder()
+	        		.username("Username")
+	        		.password("PasswordEncoded")
+	        		.fullname("FullnameUpdate")
+	        		.role("USER")
+	        		.build();
+	        
+	   when(userRepository
+			   .findById(anyInt()))
+	   .thenReturn(java.util.Optional.ofNullable(testUser1));
+	   
+       when(userMapper
+    		   .toUser(any(UserDTO.class)))
+       .thenReturn(new User("Username", "Password&1", "FullnameUpdate", "USER"));
+       
+       when(passwordEncoder
+    		   .encode(anyString()))
+       .thenReturn("PasswordEncoded");
+       
+       when(userRepository
+    		   .save(any(User.class)))
+       .thenReturn(userUpdated);
+       
+       when(userMapper
+    		   .toUserDTO(any(User.class)))
+       .thenReturn(userUpdatedDTO);
+
+        }
+
+	// *******************************************************************	
+    
+    @DisplayName("Check <Execution Order>"
+    		+ " - Given a User,"
+    		+ " when UPDATE USER action request,"
+    		+ " then all steps are executed in correct order and number of expected times")    
+    @Test
+    public void testUpdateUserExecutionOrderCheck() throws Exception {
+		
+
+        userService.updateUser(1, new UserDTO("Username", "Password&1", "FullnameUpdate", "USER"));
+
+        InOrder inOrder = inOrder(userRepository, passwordEncoder, userMapper);
+        inOrder.verify(userRepository).findById(anyInt());
+        inOrder.verify(passwordEncoder).encode(anyString());
+        inOrder.verify(userRepository).save(any(User.class));
+        inOrder.verify(userMapper).toUserDTO(any(User.class));
+        
+        verify(userRepository, times(1)).findById(anyInt());
+        verify(passwordEncoder, times(1)).encode(anyString());
+        verify(userRepository, times(1)).save(any(User.class));
+        verify(userMapper, times(1)).toUserDTO(any(User.class));
+    }
+
+    
+	// *******************************************************************	
+    }
+
+    
+    // *******************************************************************	
+}
