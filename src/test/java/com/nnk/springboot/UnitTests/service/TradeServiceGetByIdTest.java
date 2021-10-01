fe6a7b88dfa2e3031de2f9e46946a7948688f065
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-01 13:44:39 +0200

    [UNIT TEST] SERVICE-GetTradeByID Test Impl <EXCEPTION> => TEST PASSED - TRADE ID NOT FOUND

--- src/test/java/com/nnk/springboot/UnitTests/service/TradeServiceGetByIdTest.java
+++ src/test/java/com/nnk/springboot/UnitTests/service/TradeServiceGetByIdTest.java
@@ -3,6 +3,7 @@ package com.nnk.springboot.UnitTests.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.inOrder;
@@ -23,6 +24,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nnk.springboot.domain.Trade;
 import com.nnk.springboot.dto.TradeDTO;
+import com.nnk.springboot.exception.DataNotFoundException;
 import com.nnk.springboot.repository.TradeRepository;
 import com.nnk.springboot.service.TradeService;
 import com.nnk.springboot.util.TradeMapper;
@@ -143,7 +145,29 @@ class TradeServiceGetByIdTest {
 	    assertEquals("Account", result.getAccount());
 	    assertEquals(1, result.getTradeId());
     }  
-    
+  }
 
-    }
+    
+	
+  	// *******************************************************************	
+  	
+      @DisplayName("ERROR GET EXISTING USER by ID for non existing USER data"
+      		+ " - Given a non existing USER,"
+      		+ " when GET USER By ID action request,"
+      		+ " then USER entry should respond"
+      		+ " with Data Not Found Exception")
+  	@Test
+  	public void testGetTradeByIdNonExistingTradeData() throws Exception {
+
+      	when(tradeRepository
+      			.findById(anyInt()))
+      	.thenReturn(java.util.Optional.empty());
+      
+      	// WHEN // THEN
+      	assertThrows(DataNotFoundException.class, ()
+          		-> tradeService.getTradeById(1));
+  	}
+      
+  	// *******************************************************************	  
+
 }
