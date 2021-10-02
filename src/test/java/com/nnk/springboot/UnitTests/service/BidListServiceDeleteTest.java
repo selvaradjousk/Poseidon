0e150bb69bab652d836c4877d1b2af4d2a6634ac
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-02 11:30:20 +0200

    [UNIT TEST] SERVICE - BIDLIST DELETE <EXEC ORDER> => RUN FAILED

---
+++ src/test/java/com/nnk/springboot/UnitTests/service/BidListServiceDeleteTest.java
@@ -0,0 +1,99 @@
+package com.nnk.springboot.UnitTests.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.dto.BidListDTO;
+import com.nnk.springboot.exception.DataNotFoundException;
+import com.nnk.springboot.repository.BidListRepository;
+import com.nnk.springboot.service.BidListService;
+import com.nnk.springboot.util.BidListMapper;
+
+@DisplayName("BidList Service DELETE - UNIT TESTS")
+@ExtendWith(MockitoExtension.class)
+class BidListServiceDeleteTest {
+
+    @InjectMocks
+    private BidListService bidListService;
+
+    @Mock
+    private BidListRepository bidListRepository;
+    
+    @Mock
+    private BidListMapper bidListMapper;
+
+    @Mock
+    private ObjectMapper objectMapper;
+	
+    private static BidListDTO testBidListDTO1;
+    
+    private static BidList testBidList1;
+    
+    
+    @BeforeEach
+    public void setUp() {
+        objectMapper = new ObjectMapper();
+        testBidListDTO1 = BidListDTO.builder()
+        		.bidListId(1)
+        		.account("Account")
+        		.type("Type")
+        		.bidQuantity(10.0)
+        		.build();
+        
+       
+        testBidList1 = BidList.builder()
+        		.bidListId(1)
+        		.account("Account")
+        		.type("Type")
+        		.bidQuantity(10.0)
+        		.build();
+        
+       
+    }
+
+	// *******************************************************************	
+    
+	@DisplayName("Delete BidList - "
+			+ "GIVEN a BIDLIST  "
+			+ "WHEN Requested DELETE BidList"
+			+ "THEN returns expected bidList deleted")	    
+    @Test
+    public void testDeleteBidList() throws Exception {
+	
+        // GIVEN
+		 when(bidListRepository
+				 .findById(anyInt()))
+		 .thenReturn(java.util.Optional.ofNullable(testBidList1));
+		 
+        // WHEN
+		 bidListService.deleteBidList(1);
+        
+        // THEN
+	       InOrder inOrder = inOrder(bidListRepository);
+	       inOrder.verify(bidListRepository).findById(anyInt());
+	       inOrder.verify(bidListRepository).deleteById(anyInt());
+	       
+	       verify(bidListRepository, times(1)).findById(anyInt());
+	       verify(bidListRepository, times(1)).deleteById(anyInt());
+    
+	}
+   
+
+	// *******************************************************************	
+
+}
