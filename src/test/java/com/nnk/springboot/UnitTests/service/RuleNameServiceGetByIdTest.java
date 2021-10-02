bb55dd4fe42062e6415311ed614d1411e81aecb9
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-02 18:44:17 +0200

    [UNIT TEST] SERVICE - RULENAME GETByID <EXEC ORDER> => RUN FAILED

---
+++ src/test/java/com/nnk/springboot/UnitTests/service/RuleNameServiceGetByIdTest.java
@@ -0,0 +1,121 @@
+package com.nnk.springboot.UnitTests.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nnk.springboot.domain.RuleName;
+import com.nnk.springboot.dto.RuleNameDTO;
+import com.nnk.springboot.exception.DataNotFoundException;
+import com.nnk.springboot.repository.RuleNameRepository;
+import com.nnk.springboot.service.RuleNameService;
+import com.nnk.springboot.util.RuleNameMapper;
+
+@DisplayName("RuleName Service GET BY ID - UNIT TESTS")
+@ExtendWith(MockitoExtension.class)
+class RuleNameServiceGetByIdTest {
+
+    @InjectMocks
+    private RuleNameService ruleNameService;
+
+    @Mock
+    private RuleNameRepository ruleNameRepository;
+    
+    @Mock
+    private RuleNameMapper ruleNameMapper;
+
+    @Mock
+    private ObjectMapper objectMapper;
+	
+    private static RuleNameDTO testRuleNameDTO1;
+    
+    private static RuleName testRuleName1;
+    
+    
+    @BeforeEach
+    public void setUp() {
+        objectMapper = new ObjectMapper();
+        testRuleNameDTO1 = RuleNameDTO.builder()
+        		.name("RuleName1")
+        		.description("Description1")
+        		.json("JSON1")
+        		.template("template1")
+        		.sqlStr("SqlStr1")
+        		.sqlPart("SqlPart1")
+        		.build();
+        
+       
+        testRuleName1 = RuleName.builder()
+        		.name("RuleName1")
+        		.description("Description1")
+        		.json("JSON1")
+        		.template("template1")
+        		.sqlStr("SqlStr1")
+        		.sqlPart("SqlPart1")
+        		.build();
+        
+       
+    }
+  
+    // ***********************************************************************************
+
+    @DisplayName("Test GET RULENAME BY IDENTITY")
+    @Nested
+    class TestGetRuleNameById  {  
+    	
+        @BeforeEach
+        public void init() {
+        	
+            when(ruleNameRepository
+            		.findById(anyInt()))
+            .thenReturn(java.util.Optional.ofNullable(testRuleName1));
+            
+            when(ruleNameMapper
+            		.toRuleNameDTO(any(RuleName.class)))
+            .thenReturn(testRuleNameDTO1);  
+
+        }
+
+	// *******************************************************************	
+    
+    @DisplayName("Check <Execution Order>"
+    		+ " - Given a RuleName,"
+    		+ " when GET RULENAME BY ID action request,"
+    		+ " then all steps are executed in correct order and number of expected times")    
+    @Test
+    public void testGetRuleNameByIdExecutionOrderCheck() throws Exception {
+		
+
+    	ruleNameService.getRuleNameById(1);
+
+        InOrder inOrder = inOrder(ruleNameRepository, ruleNameMapper);
+        inOrder.verify(ruleNameRepository).findById(anyInt());
+        inOrder.verify(ruleNameMapper).toRuleNameDTO(any(RuleName.class));
+        
+        verify(ruleNameRepository, times(1)).findById(anyInt());
+        verify(ruleNameMapper, times(1)).toRuleNameDTO(any(RuleName.class));
+    }
+
+
+  }
+
+  
+}
