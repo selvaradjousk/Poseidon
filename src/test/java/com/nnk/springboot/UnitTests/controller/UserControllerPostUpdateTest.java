9e302fc637ecf34794bab7b10cffc8c20f388f38
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-03 19:56:51 +0200

    [UNIT TEST] CONTROLLER - USER POST /user/update/{id} < ROLE WITH SYMBOLS> => TEST PASSED

--- src/test/java/com/nnk/springboot/UnitTests/controller/UserControllerPostUpdateTest.java
+++ src/test/java/com/nnk/springboot/UnitTests/controller/UserControllerPostUpdateTest.java
@@ -605,6 +605,43 @@ class UserControllerPostUpdateTest {
     }
 
     // ********************************************************************
-  
+
+    
+    
+
+    @DisplayName("Url request /user/update/{id} - Role WithSymbols- "
+    		+ " - Given a User - Role WithSymbols -,"
+    		+ " when POST /user/update/{id} action request,"
+    		+ " then returns error & redirect /user/update page")    
+    @Test
+    public void testPostUserUpdateWithRoleWithSymbols() throws Exception {
+    	when(userService.getAllUser()).thenReturn(userDTOList);
+//    	when(userService.addUser(any(UserDTO.class))).thenReturn(any(UserDTO.class));
+        
+    	MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/user/update/1")
+        .sessionAttr("userDTO", testUserDTO1)
+        .param("username",  testUserDTO1.getUsername())
+        .param("password", testUserDTO1.getPassword())
+        .param("fullname", testUserDTO1.getFullname())
+        .param("role", "Role!!&&&"))
+        .andExpect(model().hasErrors())
+        .andExpect(model().size(1))
+        .andExpect(model().attributeExists("userDTO"))
+        .andExpect(view().name("user/update"))
+        .andExpect(status().is(200))
+        .andReturn();
+
+        verify(userService, times(0)).getAllUser();
+        verify(userService, times(0)).updateUser(anyInt(), any(UserDTO.class));
+
+        String content = result.getResponse().getContentAsString();
+        
+        assertThat(content).contains("Should be alphabets and minimum more than 2 characters");
+       
+    }
+
+    // ********************************************************************
+    
+      
     
 }
