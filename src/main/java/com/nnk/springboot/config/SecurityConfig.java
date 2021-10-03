a0c07a886bcb84806b22e1f074b48156fbc2d6df
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-03 11:37:29 +0200

    [FEAT] CONFIG - SecurityConfig ==> <IMPLEMENTATION>

---
+++ src/main/java/com/nnk/springboot/config/SecurityConfig.java
@@ -0,0 +1,134 @@
+package com.nnk.springboot.config;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+
+
+    // ************************************************************************
+    // ************************************************************************
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+
+    	log.info("login password Encoder Bcrypt - function called");
+
+        return new BCryptPasswordEncoder();
+    }
+    // ************************************************************************
+    // ************************************************************************
+
+
+
+    // ************************************************************************
+    // ************************************************************************
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    	log.info("auth.userDetailsService(userDetailsService) - function called");
+
+    	auth.userDetailsService(userDetailsService);
+
+    	log.info("auth.userDetailsService(userDetailsService) - function executed");
+
+    }
+    // ************************************************************************
+    // ************************************************************************
+
+
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception{
+
+        // ************************************************************************
+    	http.authorizeRequests()
+        // ************************************************************************
+
+                .antMatchers("/")
+                	.permitAll()
+
+                .antMatchers("/css/**")
+                	.permitAll()
+
+                .antMatchers("/images/**")
+                	.permitAll()
+
+                .antMatchers("/register/**")
+                	.permitAll()
+
+                .antMatchers("/admin/*")
+                	.hasAuthority("ADMIN")
+                
+                .antMatchers("/user/*")
+                	.hasAuthority("ADMIN")
+                
+                .anyRequest().authenticated();
+
+
+        // ************************************************************************    	
+    	http.formLogin()
+        // ************************************************************************
+        		.loginPage("/login")
+        		// redirect the user to the home page on login success
+
+        		.defaultSuccessUrl("/bidList/list")
+        		// redirect the user back to the login page on error login
+
+        		.failureUrl("/login?error=true").permitAll();
+
+
+        //      http
+        //				.oauth2Login()
+        //      		.defaultSuccessUrl("/home", true)
+        //      		.loginPage("/login");
+
+        // https://www.baeldung.com/spring-security-logout
+        // https://www.baeldung.com/spring-security-manual-logout
+        
+        // ************************************************************************
+        http.logout()
+        // ************************************************************************
+        		// list of cookies to be deleted when the user logs out
+        		.deleteCookies("JSESSIONID")
+        		// Spring security default logout value
+        		.logoutUrl("/logout")
+        		.logoutSuccessUrl("/login");
+
+
+     // ************************************************************************
+        // Cross-Site Request Forgery (CSRF) is an attack that forces
+        // authenticated users to submit a request to a Web application
+        // against which they are currently authenticated. CSRF attacks
+        // exploit the trust a Web application has in an authenticated user.
+
+        // CSRF protection is enabled by default in the Java configuration.
+        // https://stackoverflow.com/questions/52363487/what-is-the
+        // -reason-to-disable-csrf-in-spring-boot-web-application
+
+        // CSRF - disable it because it was interfering with our existing
+        // authentication mechanism.
+        http.csrf().disable();
+     // ************************************************************************
+    }
+
+
+}
