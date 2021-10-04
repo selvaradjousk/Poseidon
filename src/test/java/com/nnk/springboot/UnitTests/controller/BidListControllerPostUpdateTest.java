bbccd5932b1b53622302c7b5c13e15885ed5f9bc
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-04 22:11:02 +0200

    [UNIT TEST] CONTROLLER - BIDLIST POST /bidList/update/{id} <TYPE >30 characters> => PASSED

--- src/test/java/com/nnk/springboot/UnitTests/controller/BidListControllerPostUpdateTest.java
+++ src/test/java/com/nnk/springboot/UnitTests/controller/BidListControllerPostUpdateTest.java
@@ -253,5 +253,40 @@ class BidListControllerPostUpdateTest {
     // ********************************************************************
 
 
+    @DisplayName("Url request /bidList/update/{id} - Type MoreThanThiryCharacters - "
+    		+ " - Given a BidList - Type MoreThanThiryCharacters,"
+    		+ " when POST /bidList/update/{id} action request,"
+    		+ " then returns error & redirect /bidList/update/{id} page")    
+    @Test
+    public void testPostBidListUpdateWithMoreThanThiryCharacters() throws Exception {
+    	when(bidListService.getAllBidList()).thenReturn(bidListDTOList);
+//    	when(bidListService.updateBidList(anyInt(), any(BidListDTO.class))).thenReturn(any(BidListDTO.class));
+        
+    	MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/bidList/update/1")
+        .sessionAttr("bidListDTO", testBidListDTO1)
+        .param("account", testBidListDTO1.getAccount())
+        .param("type", "TypeTypeTypeTypeTypeTypeTypeType")
+        .param("bidQuantity", testBidListDTO1.getBidQuantity().toString()))
+        .andExpect(model().hasErrors())
+        .andExpect(model().size(2))
+        .andExpect(model().attributeExists("bidListDTO"))
+        .andExpect(view().name("bidList/update"))
+        .andExpect(status().is(200))
+        .andReturn();
+
+        verify(bidListService, times(0)).getAllBidList();
+        verify(bidListService, times(0)).updateBidList(anyInt(), any(BidListDTO.class));
+        
+
+        String content = result.getResponse().getContentAsString();
+        
+      assertThat(content).contains("The maximum length for type should be 30 characters");
+
+    }
+
+    // ********************************************************************
+
+
+
     
 }
