c4694b85f0763d210c437c9a969fe10a70e0c944
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-04 13:02:02 +0200

    [UNIT TEST] CONTROLLER TRADE - GET /trade/update/{id}  <VALID INPUT> => RUN FAILED

---
+++ src/test/java/com/nnk/springboot/UnitTests/controller/TradeControllerGetUpdateTest.java
@@ -0,0 +1,96 @@
+package com.nnk.springboot.UnitTests.controller;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nnk.springboot.controller.TradeController;
+import com.nnk.springboot.dto.TradeDTO;
+import com.nnk.springboot.service.TradeService;
+
+@DisplayName("Controller < TRADE - GET UPDATE> - UNIT TESTS")
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(TradeController.class)
+class TradeControllerGetUpdateTest {
+
+
+    @MockBean
+    private TradeService tradeService;
+
+    @MockBean
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private ObjectMapper objectMapper;
+	
+    private static TradeDTO testTradeDTO1, testTradeDTO2;
+ 
+
+    @BeforeEach
+    public void setUp() {
+
+        objectMapper = new ObjectMapper();
+        testTradeDTO1 = TradeDTO.builder()
+        		.tradeId(1)
+        		.account("Account")
+        		.type("Type")
+        		.buyQuantity(10.0)
+        		.build();
+        
+        testTradeDTO2 = TradeDTO.builder()
+        		.tradeId(2)
+        		.account("Account")
+        		.type("Type")
+        		.buyQuantity(10.0)
+        		.build();
+
+        
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+    // ********************************************************************
+
+
+    
+    @DisplayName(" Url request /trade/update/{id} - "
+    		+ " - Given a Trade,"
+    		+ " when GET /trade/update/{id} action request,"
+    		+ " then returns trade ADD page")    
+    @Test
+    public void testGetTradeUpdateById() throws Exception {
+    	 when(tradeService.getTradeById(1)).thenReturn(testTradeDTO1);
+        mockMvc.perform(MockMvcRequestBuilders.get("/trade/update/1"))
+                .andExpect(model().attributeExists("tradeDTO"))
+                .andExpect(model().size(1))
+                .andExpect(view().name("trade/update"))
+                .andExpect(status().isOk());
+
+        verify(tradeService).getTradeById(1);
+    }
+
+    // ********************************************************************
+
+    
+    
+}
