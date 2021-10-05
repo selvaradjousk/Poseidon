c196455499bd29375d986889e8913b57bd68fad0
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-05 19:57:17 +0200

    [UNIT TEST] CONTROLLER - RULENAME POST /ruleName/update/{id} <Description empty> => PASSED

--- src/test/java/com/nnk/springboot/UnitTests/controller/RuleNameControllerPostUpdateTest.java
+++ src/test/java/com/nnk/springboot/UnitTests/controller/RuleNameControllerPostUpdateTest.java
@@ -279,4 +279,44 @@ class RuleNameControllerPostUpdateTest {
 
     // ********************************************************************
 
+    @DisplayName(" Url request /ruleName/update/{id} - Empty Description "
+    		+ " - Given a RuleName - Empty Description,"
+    		+ " when POST /ruleName/update/{id} action request,"
+    		+ " then returns error & redirect /ruleName/update page")    
+    @Test
+    public void testPostRuleNameValidateEmptyDescription() throws Exception {
+    	when(ruleNameService.getAllRuleName()).thenReturn(ruleNameDTOList);
+
+    	when(ruleNameService
+    			.updateRuleName(anyInt(), any(RuleNameDTO.class)))
+     	.thenReturn(testRuleNameDTO1);
+        
+        
+    	MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/ruleName/update/1")
+		        .sessionAttr("ruleNameDTO", testRuleNameDTO1)
+		        .param("id", testRuleNameDTO1.getId().toString())
+		        .param("name", testRuleNameDTO1.getName())
+		        .param("description", "")
+		        .param("json", testRuleNameDTO1.getJson())
+		        .param("template", testRuleNameDTO1.getTemplate())
+		        .param("sqlStr", testRuleNameDTO1.getSqlStr())
+		        .param("sqlPart", testRuleNameDTO1.getSqlPart()))
+		        .andExpect(model().hasErrors())
+		        .andExpect(model().size(1))
+		        .andExpect(model().attributeExists("ruleNameDTO"))
+		        .andExpect(view().name("ruleName/update"))
+		        .andExpect(status().is(200))
+		        .andReturn();
+
+        verify(ruleNameService, times(0)).getAllRuleName();
+        verify(ruleNameService, times(0)).updateRuleName(anyInt(), any(RuleNameDTO.class));
+
+        String content = result.getResponse().getContentAsString();
+        
+        assertThat(content).contains("Description is mandatory");
+        assertThat(content).contains("Should be alphanumeric and minimum more than 2 characters");
+    }
+
+    // ********************************************************************
+
 }
