3d1cb4be81d67a06fd3a474ddb707d46e004bd70
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-05 17:03:52 +0200

    [UNIT TEST] CONTROLLER - RULENAME POST /ruleName/validate <Template >512 characters> => PASSED

--- src/test/java/com/nnk/springboot/UnitTests/controller/RuleNameControllerPostValidateTest.java
+++ src/test/java/com/nnk/springboot/UnitTests/controller/RuleNameControllerPostValidateTest.java
@@ -415,4 +415,50 @@ class RuleNameControllerPostValidateTest {
 
     // ********************************************************************
 
+
+    @DisplayName(" Url request /ruleName/validate - Template length > 512 "
+    		+ " - Given a RuleName - Template length > 512,"
+    		+ " when POST /ruleName/validate action request,"
+    		+ " then returns error & redirect /ruleName/add page")    
+    @Test
+    public void testPostRuleNameValidateTemplateWith512MoreCharacters() throws Exception {
+    	when(ruleNameService.getAllRuleName()).thenReturn(ruleNameDTOList);
+//    	when(ruleNameService.addRuleName(any(RuleNameDTO.class))).thenReturn(any(RuleNameDTO.class));
+        
+    	MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/ruleName/validate")
+		        .sessionAttr("ruleNameDTO", testRuleNameDTO1)
+		        .param("id", testRuleNameDTO1.getId().toString())
+			.param("name", testRuleNameDTO1.getName())
+		        .param("description", testRuleNameDTO1.getDescription())
+		        .param("json", testRuleNameDTO1.getJson())
+		        .param("template", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+		        		+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+		        		+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+		        		+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+		        		+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+		        		+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+		        		+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+		        		+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+		        		+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+		        		+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+		        		+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+		        .param("sqlStr", testRuleNameDTO1.getSqlStr())
+		        .param("sqlPart", testRuleNameDTO1.getSqlPart()))
+		        .andExpect(model().hasErrors())
+		        .andExpect(model().size(1))
+		        .andExpect(model().attributeExists("ruleNameDTO"))
+		        .andExpect(view().name("ruleName/add"))
+		        .andExpect(status().is(200))
+		        .andReturn();
+
+        verify(ruleNameService, times(0)).getAllRuleName();
+        verify(ruleNameService, times(0)).addRuleName(any(RuleNameDTO.class));
+
+        String content = result.getResponse().getContentAsString();
+        
+        assertThat(content).contains("The maximum length for template can be 512 characters");
+    }
+
+    // ********************************************************************
+
 }
