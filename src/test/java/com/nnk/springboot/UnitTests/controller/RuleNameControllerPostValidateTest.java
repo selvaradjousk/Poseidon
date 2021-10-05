50dc08e157b97ace9ad73a2cbf2b25244c61cde5
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-05 17:07:26 +0200

    [UNIT TEST] CONTROLLER - RULENAME POST /ruleName/validate <sqlStr >125 characters> => PASSED

--- src/test/java/com/nnk/springboot/UnitTests/controller/RuleNameControllerPostValidateTest.java
+++ src/test/java/com/nnk/springboot/UnitTests/controller/RuleNameControllerPostValidateTest.java
@@ -461,4 +461,41 @@ class RuleNameControllerPostValidateTest {
 
     // ********************************************************************
 
+    @DisplayName(" Url request /ruleName/validate - SqlStr length > 125 "
+    		+ " - Given a RuleName - SqlStr length > 125,"
+    		+ " when POST /ruleName/validate action request,"
+    		+ " then returns error & redirect /ruleName/add page")    
+    @Test
+    public void testPostRuleNameValidateSqlStrWith125MoreCharacters() throws Exception {
+    	when(ruleNameService.getAllRuleName()).thenReturn(ruleNameDTOList);
+//    	when(ruleNameService.addRuleName(any(RuleNameDTO.class))).thenReturn(any(RuleNameDTO.class));
+        
+    	MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/ruleName/validate")
+		        .sessionAttr("ruleNameDTO", testRuleNameDTO1)
+		        .param("id", testRuleNameDTO1.getId().toString())
+			.param("name", testRuleNameDTO1.getName())
+		        .param("description", testRuleNameDTO1.getDescription())
+		        .param("json", testRuleNameDTO1.getJson())
+		        .param("template", testRuleNameDTO1.getTemplate())
+		        .param("sqlStr", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+		        		+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+		        		+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+		        .param("sqlPart", testRuleNameDTO1.getSqlPart()))
+		        .andExpect(model().hasErrors())
+		        .andExpect(model().size(1))
+		        .andExpect(model().attributeExists("ruleNameDTO"))
+		        .andExpect(view().name("ruleName/add"))
+		        .andExpect(status().is(200))
+		        .andReturn();
+
+        verify(ruleNameService, times(0)).getAllRuleName();
+        verify(ruleNameService, times(0)).addRuleName(any(RuleNameDTO.class));
+
+        String content = result.getResponse().getContentAsString();
+        
+        assertThat(content).contains("The maximum length for sqlStr can be 125 characters");
+    }
+
+    // ********************************************************************
+
 }
