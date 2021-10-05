5dba5eb776ba07f6a351cc4f97e3d5eed9fd45b6
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-05 17:58:53 +0200

    [UNIT TEST] CONTROLLER - RATING POST /rating/validate <ID valid> => PASSED

---
+++ src/test/java/com/nnk/springboot/UnitTests/controller/RatingControllerPostValidateTest.java
@@ -0,0 +1,145 @@
+package com.nnk.springboot.UnitTests.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nnk.springboot.controller.RatingController;
+import com.nnk.springboot.dto.RatingDTO;
+import com.nnk.springboot.dto.RuleNameDTO;
+import com.nnk.springboot.service.RatingService;
+
+	@DisplayName("Controller < RATING > -VALIDATE UNIT TESTS")
+	@ExtendWith(SpringExtension.class)
+	@WebMvcTest(RatingController.class)
+	class RatingControllerPostValidateTest {
+
+
+	    @MockBean
+	    private RatingService ratingService;
+
+	    @MockBean
+	    private UserDetailsService userDetailsService;
+
+	    @Autowired
+	    private MockMvc mockMvc;
+
+	    @Autowired
+	    private WebApplicationContext context;
+
+	    private ObjectMapper objectMapper;
+		
+	    private static RatingDTO testRatingDTO1, testRatingDTO2;
+	 
+	    private static List<RatingDTO> ratingDTOList;
+
+	    @BeforeEach
+	    public void setUp() {
+
+	        objectMapper = new ObjectMapper();
+	        testRatingDTO1 = RatingDTO.builder()
+	        		.id(1)
+	        		.moodysRating("MoodyRating1")
+	        		.sandPRating("SandPRating1")
+	        		.fitchRating("FitchRating1")
+	        		.orderNumber(1)
+	        		.build();
+	        
+	        testRatingDTO2 = RatingDTO.builder()
+	        		.id(2)
+	        		.moodysRating("MoodyRating2")
+	        		.sandPRating("SandPRating2")
+	        		.fitchRating("FitchRating2")
+	        		.orderNumber(2)
+	        		.build();
+	        
+	        ratingDTOList = Arrays.asList(testRatingDTO1, testRatingDTO2); 
+	        
+	        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+	    }
+	    
+	  	// ********************************************************************
+
+
+	    @DisplayName(" Url request /rating/validate - "
+	    		+ " - Given a Rating,"
+	    		+ " when POST /rating/validate action request,"
+	    		+ " then returns redirect /rating/validate page")    
+	    @Test
+	    public void testPostRatingValidate() throws Exception {
+
+	    	when(ratingService
+	    			.addRating(any(RatingDTO.class)))
+	    	.thenReturn(testRatingDTO1);
+	        
+	        mockMvc.perform(MockMvcRequestBuilders.post("/rating/validate")
+	        .sessionAttr("ratingDTO", testRatingDTO1)
+			        .param("id", testRatingDTO1.getId().toString())
+			        .param("moodysRating", testRatingDTO1.getMoodysRating())
+			        .param("sandPRating", testRatingDTO1.getSandPRating())
+			        .param("fitchRating", testRatingDTO1.getFitchRating()))
+			        .andExpect(model().hasNoErrors())
+			        .andExpect(model().size(0))
+			        .andExpect(model().attributeDoesNotExist("ratingDTO"))
+			        .andExpect(redirectedUrl("/rating/list"))
+			        .andExpect(status().is(302));
+
+	        verify(ratingService, times(1)).addRating(any(RatingDTO.class));
+	    }
+
+	    // ********************************************************************
+
+	    
+
+	    @DisplayName(" Url request /rating/validate - Id Negative "
+	    		+ " - Given a Rating, Id Negative"
+	    		+ " when POST /rating/validate action request,"
+	    		+ " then returns error & redirect /rating/add page")    
+	    @Test
+	    public void testPostRatingValidateIdNegative() throws Exception {
+
+	    	when(ratingService
+	    			.addRating(any(RatingDTO.class)))
+	    	.thenReturn(any(RatingDTO.class));
+	        
+	        mockMvc.perform(MockMvcRequestBuilders.post("/rating/validate")
+	        .sessionAttr("ratingDTO", testRatingDTO1)
+	        .param("id", testRatingDTO1.getId().toString())
+	        .param("moodysRating", testRatingDTO1.getMoodysRating())
+	        .param("sandPRating", testRatingDTO1.getSandPRating())
+	        .param("fitchRating", testRatingDTO1.getFitchRating()))
+			        .andExpect(model().hasNoErrors())
+			        .andExpect(model().size(0))
+			        .andExpect(model().attributeDoesNotExist("ratingDTO"))
+			        .andExpect(redirectedUrl("/rating/list"))
+			        .andExpect(status().is(302));
+
+	        verify(ratingService, times(1)).addRating(any(RatingDTO.class));
+	    }
+
+	    // ********************************************************************
+	    	    
+	    
+}
