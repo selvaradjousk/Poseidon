8beb681002470addb2be9f59d7e6ae65064c9bb3
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-05 19:34:07 +0200

    [UNIT TEST] CONTROLLER - RATING POST /rating/update/{id} <FitchRating +Symbols> => PASSED

--- src/test/java/com/nnk/springboot/UnitTests/controller/RatingControllerPostUpdateTest.java
+++ src/test/java/com/nnk/springboot/UnitTests/controller/RatingControllerPostUpdateTest.java
@@ -432,4 +432,43 @@ import com.nnk.springboot.service.RatingService;
 
 		// ********************************************************************
 
+
+	    @DisplayName(" Url request /rating/update/{id} - FitchRating With Symbols "
+	    		+ " - Given a Rating - FitchRating With Symols,"
+	    		+ " when POST /rating/update/{id} action request,"
+	    		+ " then returns error & redirect /rating/add page")    
+	    @Test
+	    public void testPostRatingValidateFitchRatingWithSymbols() throws Exception {
+
+	    	when(ratingService.getAllRating()).thenReturn(ratingDTOList);
+
+	    	when(ratingService
+	    			.updateRating(anyInt(), any(RatingDTO.class)))
+	    	.thenReturn(testRatingDTO1);
+	        
+	        
+	    	MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/rating/update/1")
+			        .sessionAttr("ratingDTO", testRatingDTO1)
+			        .param("id", testRatingDTO1.getId().toString())
+			        .param("moodysRating", testRatingDTO1.getMoodysRating())
+			        .param("sandPRating", testRatingDTO1.getSandPRating())
+			        .param("fitchRating", "&&&aaa"))
+			        .andExpect(model().hasErrors())
+			        .andExpect(model().size(2))
+			        .andExpect(model().attributeExists("ratingDTO"))
+			        .andExpect(view().name("rating/update"))
+			        .andExpect(status().is(200))
+			        .andReturn();
+
+
+	        verify(ratingService, times(0)).getAllRating();
+	        verify(ratingService, times(0)).updateRating(anyInt(), any(RatingDTO.class));
+
+	        String content = result.getResponse().getContentAsString();
+	        
+	        assertThat(content).contains("Should be alphanumeric and minimum more than 2 characters");
+	    }
+
+	    // ********************************************************************
+
 }
