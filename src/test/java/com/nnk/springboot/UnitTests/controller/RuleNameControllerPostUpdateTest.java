10d5ea0c2eacdd6a593f4736d02a640a95d82519
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-05 19:58:17 +0200

    [UNIT TEST] CONTROLLER - RULENAME POST /ruleName/update/{id} <Description  > 125 characters> => PASSED

--- src/test/java/com/nnk/springboot/UnitTests/controller/RuleNameControllerPostUpdateTest.java
+++ src/test/java/com/nnk/springboot/UnitTests/controller/RuleNameControllerPostUpdateTest.java
@@ -360,4 +360,47 @@ class RuleNameControllerPostUpdateTest {
 
     // ********************************************************************
 
+
+
+    @DisplayName(" Url request /ruleName/update/{id} - Description length > 125 "
+    		+ " - Given a RuleName - Description length > 125,"
+    		+ " when POST /ruleName/update/{id} action request,"
+    		+ " then returns error & redirect /ruleName/update page")    
+    @Test
+    public void testPostRuleNameValidateDescriptionWith125MoreCharecters() throws Exception {
+    	when(ruleNameService.getAllRuleName()).thenReturn(ruleNameDTOList);
+
+    	when(ruleNameService
+    			.updateRuleName(anyInt(), any(RuleNameDTO.class)))
+     	.thenReturn(testRuleNameDTO1);
+        
+        
+    	MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/ruleName/update/1")
+		        .sessionAttr("ruleNameDTO", testRuleNameDTO1)
+		        .param("id", testRuleNameDTO1.getId().toString())
+			.param("name", testRuleNameDTO1.getName())
+		        .param("description", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+		        		+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+		        		+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+		        .param("json", testRuleNameDTO1.getJson())
+		        .param("template", testRuleNameDTO1.getTemplate())
+		        .param("sqlStr", testRuleNameDTO1.getSqlStr())
+		        .param("sqlPart", testRuleNameDTO1.getSqlPart()))
+		        .andExpect(model().hasErrors())
+		        .andExpect(model().size(1))
+		        .andExpect(model().attributeExists("ruleNameDTO"))
+		        .andExpect(view().name("ruleName/update"))
+		        .andExpect(status().is(200))
+		        .andReturn();
+
+        verify(ruleNameService, times(0)).getAllRuleName();
+        verify(ruleNameService, times(0)).updateRuleName(anyInt(), any(RuleNameDTO.class));
+
+        String content = result.getResponse().getContentAsString();
+        
+        assertThat(content).contains("The maximum length for description can be 125 characters");
+    }
+
+    // ********************************************************************
+
 }
