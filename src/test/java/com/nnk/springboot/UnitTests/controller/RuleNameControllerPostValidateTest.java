dc86364a39106091a0807b8beed81b8327d9bcb6
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-05 16:48:10 +0200

    [UNIT TEST] CONTROLLER - RULENAME POST /ruleName/validate <NAME > 125 characters> => PASSED

--- src/test/java/com/nnk/springboot/UnitTests/controller/RuleNameControllerPostValidateTest.java
+++ src/test/java/com/nnk/springboot/UnitTests/controller/RuleNameControllerPostValidateTest.java
@@ -227,4 +227,42 @@ class RuleNameControllerPostValidateTest {
     // ********************************************************************
 
 
+
+    @DisplayName(" Url request /ruleName/validate - Name length > 125 "
+    		+ " - Given a RuleName - Name length > 125,"
+    		+ " when POST /ruleName/validate action request,"
+    		+ " then returns error & redirect /ruleName/add page")    
+    @Test
+    public void testPostRuleNameValidateNameWith125MoreCharecters() throws Exception {
+    	when(ruleNameService.getAllRuleName()).thenReturn(ruleNameDTOList);
+//    	when(ruleNameService.addRuleName(any(RuleNameDTO.class))).thenReturn(any(RuleNameDTO.class));
+        
+    	MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/ruleName/validate")
+		        .sessionAttr("ruleNameDTO", testRuleNameDTO1)
+		        .param("id", testRuleNameDTO1.getId().toString())
+		        .param("name", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+		        		+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+		        		+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+		        .param("description", testRuleNameDTO1.getDescription())
+		        .param("json", testRuleNameDTO1.getJson())
+		        .param("template", testRuleNameDTO1.getTemplate())
+		        .param("sqlStr", testRuleNameDTO1.getSqlStr())
+		        .param("sqlPart", testRuleNameDTO1.getSqlPart()))
+		        .andExpect(model().hasErrors())
+		        .andExpect(model().size(1))
+		        .andExpect(model().attributeExists("ruleNameDTO"))
+		        .andExpect(view().name("ruleName/add"))
+		        .andExpect(status().is(200))
+		        .andReturn();
+
+        verify(ruleNameService, times(0)).getAllRuleName();
+        verify(ruleNameService, times(0)).addRuleName(any(RuleNameDTO.class));
+
+        String content = result.getResponse().getContentAsString();
+        
+        assertThat(content).contains("The maximum length for name can be 125 characters");
+    }
+
+    // ********************************************************************
+
 }
