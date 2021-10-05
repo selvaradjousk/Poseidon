2ff38a9029ca3e9cc26ae918f54dde3bb93e8a8c
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-05 15:52:04 +0200

    [FEAT] CONTROLLER - CURVEPOINT POST /curvePoint/update/{id} <VALUE -VE> => PASSED

--- src/test/java/com/nnk/springboot/UnitTests/controller/CurvePointControllerPostUpdateTest.java
+++ src/test/java/com/nnk/springboot/UnitTests/controller/CurvePointControllerPostUpdateTest.java
@@ -361,7 +361,41 @@ class CurvePointControllerPostUpdateTest {
     }
 
     // ********************************************************************
-            
+
+
+    @DisplayName("Url request /curvePoint/update/{id} - Value Negative - "
+    		+ " - Given a CurvePoint - Value Negative -,"
+    		+ " when POST /curvePoint/update/{id} action request,"
+    		+ " then returns error & redirect /curvePoint/add page")    
+    @Test
+    public void testPostCurvePointValidateWithValueNegative() throws Exception {
+    	when(curvePointService.getAllCurvePoint()).thenReturn(curvePointDTOList);
+//    	when(curvePointService..updateCurvePoint(anyInt(), any(CurvePointDTO.class))).thenReturn(any(CurvePointDTO.class));
+        
+    	MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/curvePoint/update/1")
+        .sessionAttr("curvePointDTO", testCurvePointDTO1)
+        .param("curveId", testCurvePointDTO1.getCurveId().toString())
+        .param("term", testCurvePointDTO1.getTerm().toString())
+        .param("value", "-10.0"))
+        .andExpect(model().hasErrors())
+        .andExpect(model().size(2))
+        .andExpect(model().attributeExists("curvePointDTO"))
+        .andExpect(view().name("curvePoint/update"))
+        .andExpect(status().is(200))
+        .andReturn();
+
+        verify(curvePointService, times(0)).getAllCurvePoint();
+        verify(curvePointService, times(0)).updateCurvePoint(anyInt(), any(CurvePointDTO.class));
+        
+
+        String content = result.getResponse().getContentAsString();
+        
+        assertThat(content).contains("The value must be positive");
+              
+    }
+
+    // ********************************************************************
+           
              
          
 }
