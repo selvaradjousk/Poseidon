29006a7d40f1710c902625bdedcd21fbe090428e
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-05 15:03:19 +0200

    [UNIT TEST] CONTROLLER - CURVEPOINT POST /curvePoint/validate <Term NotNull> => PASSED

--- src/test/java/com/nnk/springboot/UnitTests/controller/CurvePointControllerPostValidateTest.java
+++ src/test/java/com/nnk/springboot/UnitTests/controller/CurvePointControllerPostValidateTest.java
@@ -188,6 +188,39 @@ class CurvePointControllerPostValidateTest {
 
     // ********************************************************************
 
+
+    @DisplayName("Url request /curvePoint/validate - Term Null - "
+    		+ " - Given a CurvePoint - Term Null-,"
+    		+ " when POST /curvePoint/validate action request,"
+    		+ " then returns error & redirect /curvePoint/add page")    
+    @Test
+    public void testPostCurvePointValidateWithTermNull() throws Exception {
+    	when(curvePointService.getAllCurvePoint()).thenReturn(curvePointDTOList);
+//    	when(curvePointService.addCurvePoint(any(CurvePointDTO.class))).thenReturn(any(CurvePointDTO.class));
         
+    	MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/curvePoint/validate")
+        .sessionAttr("curvePointDTO", testCurvePointDTO1)
+        .param("curveId", testCurvePointDTO1.getCurveId().toString())
+        .param("term", "")
+        .param("value", testCurvePointDTO1.getValue().toString()))
+        .andExpect(model().hasErrors())
+        .andExpect(model().size(1))
+        .andExpect(model().attributeExists("curvePointDTO"))
+        .andExpect(view().name("curvePoint/add"))
+        .andExpect(status().is(200))
+        .andReturn();
+
+        verify(curvePointService, times(0)).getAllCurvePoint();
+        verify(curvePointService, times(0)).addCurvePoint(any(CurvePointDTO.class));
+        
+
+        String content = result.getResponse().getContentAsString();
+        
+        assertThat(content).contains("Term is mandatory");
+              
+    }
+
+    // ********************************************************************
+       
     
 }
