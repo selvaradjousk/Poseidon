a807b9774eaf2551a3f479f7c4b98a4b35642e47
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-11 22:52:31 +0200

    [INTEGRATION TESTS] GET trade/list => TESTS PASSED

--- src/test/java/com/nnk/springboot/IT/controller/TradeControllerGetListTest.java
+++ src/test/java/com/nnk/springboot/IT/controller/TradeControllerGetListTest.java
@@ -1,10 +1,11 @@
 package com.nnk.springboot.IT.controller;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -19,78 +20,82 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nnk.springboot.service.TradeService;
 
-@DisplayName("INTEGRATION TESTS - Controller < TRADE > DELETE")
+@DisplayName("INTEGRATION TESTS - Controller < TRADE > GET LIST")
 @AutoConfigureMockMvc
 @SpringBootTest
 @ActiveProfiles("test")
-class TradeControllerGetDeleteTest {
+class TradeControllerGetListTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private TradeService tradeService;
+    
     @BeforeEach
     public void setUp() {
 
     }
-
+    
   	// ********************************************************************
 
-    @DisplayName(" Url request trade/delete/{id} valid id - Without Authentication"
-    		+ " - Given a trade/delete/{id} valid id, "
-    		+ " when GET trade/delete action request,"
-    		+ " then returns delete page")    
+    @DisplayName(" Url request /trade/list - "
+    		+ " - Given a Trade List, Without Authentication"
+    		+ " when GET /trade/list action request,"
+    		+ " then returns tradeslist page")
     @Test
-    public void testGetTradeDeleteWithoutAuthentication() throws Exception {
+    public void testGetTradeListWithoutAuthentication() throws Exception {
 
-    	mockMvc.perform(MockMvcRequestBuilders.get("/trade/delete/1"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/trade/list"))
 	        .andExpect(status().is(401))
 	        .andDo(MockMvcResultHandlers.print())
 	        .andExpect(status().isUnauthorized())
 	        .andExpect(status().reason(containsString("Full authentication is required to access this resource")))
 	        .andExpect(unauthenticated());
- 
+
     }
 
-  	// ********************************************************************
+    
+    // ********************************************************************
 
     @WithMockUser(username = "admin", authorities = { "ADMIN", "USER"})
-    @DisplayName(" Url request trade/delete/{id} valid id - With Authentication "
-    		+ " - Given a trade/delete/{id} valid id, "
-    		+ " when GET trade/delete action request,"
-    		+ " then returns delete page")    
+    @DisplayName(" Url request /trade/list - With Authentication "
+    		+ " - Given a Trade List,"
+    		+ " when GET /trade/list action request,"
+    		+ " then returns tradeslist page")
     @Test
-    public void testGetTradeDeleteWithAuthentication () throws Exception {
+    public void testGetTradeListWithAuthentication() throws Exception {
 
-    	mockMvc.perform(MockMvcRequestBuilders.get("/trade/delete/1"))
-    				.andExpect(redirectedUrl("/trade/list"))
-            		.andExpect(status().isFound())
-            		.andExpect(model().hasNoErrors())
-            		.andExpect(status().is(302));
- 
+        mockMvc.perform(MockMvcRequestBuilders.get("/trade/list"))
+                .andExpect(model().attributeExists("trades"))
+                .andExpect(model().size(1))
+                .andExpect(view().name("trade/list"))
+                .andExpect(status().isOk());
+
+        assertEquals(5, (tradeService.getAllTrade()).size());
     }
 
     // ********************************************************************
 
     @WithMockUser(username = "admin", authorities = { "ADMIN", "USER"})
-    @DisplayName(" Url request trade/delete/{id} invalid id- "
-    		+ " - Given a Trade trade/delete/{id} invalid id,"
-    		+ " when GET trade/delete action request,"
-    		+ " then returns delete page")    
+    @DisplayName(" Url request /trade/list - NULL LIST "
+    		+ " - Given a Trade List,"
+    		+ " when GET /trade/list action request,"
+    		+ " then returns tradeslist page")    
     @Test
-    public void testGetTradeDeleteNull() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/trade/delete/"))
-        		.andExpect(redirectedUrl(null))
-                .andExpect(status().is(404))
-        		.andExpect(status().isNotFound());
+    public void testGetTradeListNull() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/trade/list"))
+                .andExpect(model().size(1))
+                .andExpect(view().name("trade/list"))
+                .andExpect(status().isOk());
 
     }
 
     // ********************************************************************
-   
-   
-
-
+  
 }
