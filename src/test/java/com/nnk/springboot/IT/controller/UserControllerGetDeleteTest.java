6811712da0dfdd8e6639b70ef47bf96c4aa5ebae
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-12 00:28:37 +0200

    [INTEGRATION TESTS] GET user/delete/{id} => TESTS PASSED

---
+++ src/test/java/com/nnk/springboot/IT/controller/UserControllerGetDeleteTest.java
@@ -0,0 +1,95 @@
+package com.nnk.springboot.IT.controller;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+@DisplayName("INTEGRATION TESTS - Controller < USER > DELETE")
+@AutoConfigureMockMvc
+@SpringBootTest
+@ActiveProfiles("test")
+class UserControllerGetDeleteTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() {
+
+    }
+    
+  	// ********************************************************************
+
+    
+    @DisplayName(" Url request GET user/delete/{id} valid id - Without Authentication"
+    		+ " - Given a user/delete/{id} valid id, "
+    		+ " when GET user/delete action request,"
+    		+ " then returns Error Authentication required") 
+    @Test
+    public void testGetUserDeleteWithoutAuthentication() throws Exception {
+
+    	mockMvc.perform(MockMvcRequestBuilders.get("/user/delete/1"))
+	        .andExpect(status().is(401))
+	        .andDo(MockMvcResultHandlers.print())
+	        .andExpect(status().isUnauthorized())
+	        .andExpect(status().reason(containsString("Full authentication is required to access this resource")))
+	        .andExpect(unauthenticated());
+ 
+    }
+
+    // ********************************************************************
+    
+    @WithMockUser(username = "admin", authorities = { "ADMIN", "USER"})
+    @DisplayName(" Url request user/delete/{id} valid id - With Authentication "
+    		+ " - Given a user/delete/{id} valid id, "
+    		+ " when GET user/delete action request,"
+    		+ " then returns delete page")    
+    @Test
+    public void testGetUserDeleteWithAuthentication() throws Exception {
+
+    	mockMvc.perform(MockMvcRequestBuilders.get("/user/delete/1"))
+    				.andExpect(redirectedUrl("/user/list"))
+            		.andExpect(status().isFound())
+            		.andExpect(model().hasNoErrors())
+            		.andExpect(status().is(302));
+
+
+        
+    }
+
+    // ********************************************************************
+
+    @WithMockUser(username = "admin", authorities = { "ADMIN", "USER"})
+    @DisplayName(" Url request user/delete/{id} invalid id- "
+    		+ " - Given a User user/delete/{id} invalid id,"
+    		+ " when GET user/delete action request,"
+    		+ " then returns delete page")    
+    @Test
+    public void testGetUserDeleteNull() throws Exception {
+    	
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/delete/"))
+        		.andExpect(redirectedUrl(null))
+                .andExpect(status().is(404))
+        		.andExpect(status().isNotFound());
+
+    }
+
+    // ********************************************************************
+   
+   
+}
