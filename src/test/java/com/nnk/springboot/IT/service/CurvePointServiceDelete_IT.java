051c8033fa64fc6a4712fb9983f03af162e006d5
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-12 09:08:52 +0200

    [INTEGRATION TESTS] curvePointService.deleteCurvePoint({id}) => TESTS PASSED

---
+++ src/test/java/com/nnk/springboot/IT/service/CurvePointServiceDelete_IT.java
@@ -0,0 +1,55 @@
+package com.nnk.springboot.IT.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.nnk.springboot.exception.DataNotFoundException;
+import com.nnk.springboot.service.CurvePointService;
+
+@DisplayName("Service ==> CurvePoint DELETE - UNIT TESTS")
+@SpringBootTest
+@ActiveProfiles("test")
+class CurvePointServiceDelete_IT {
+
+
+    @Autowired
+    private CurvePointService curvePointService;
+
+
+	// *******************************************************************	
+    
+    @DisplayName("Check <Delete CurvePoint>"
+		+ "GIVEN a CurvePoint  "
+		+ "WHEN Requested DELETE CurvePoint "
+		+ "THEN Deletes CurvePoint")	    
+	@Test
+	public void CurvePoint() throws Exception {
+
+    	curvePointService.deleteCurvePoint(3);
+    	// WHEN // THEN
+    	assertThrows(DataNotFoundException.class, ()
+        		-> curvePointService.deleteCurvePoint(3));
+	} 
+
+	// *******************************************************************	
+ 
+    @DisplayName("Check <Exception>"
+		+ "GIVEN a CurvePoint not exist "
+		+ "WHEN Requested DELETE CurvePoint "
+		+ "THEN throws Exception")	    
+	@Test
+	public void testDeleteCurvePointNotExists() throws Exception {
+    
+    	// WHEN // THEN
+    	assertThrows(DataNotFoundException.class, ()
+        		-> curvePointService.deleteCurvePoint(20));
+	} 
+
+	// *******************************************************************	
+
+}
