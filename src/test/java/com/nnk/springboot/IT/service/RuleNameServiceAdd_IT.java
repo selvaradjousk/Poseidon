2be1f782f79d9713db6a51a5439008ab6b67122d
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-13 10:04:33 +0200

    [INTEGRATION TESTS] ruleNameService.deleteRuleName({id}) => TESTS PASSED

--- src/test/java/com/nnk/springboot/IT/service/RuleNameServiceAdd_IT.java
+++ src/test/java/com/nnk/springboot/IT/service/RuleNameServiceAdd_IT.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
