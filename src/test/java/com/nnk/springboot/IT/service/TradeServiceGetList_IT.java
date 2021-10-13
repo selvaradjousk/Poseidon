0dc8a3bc5f4a388f09de94008e0a8a3ca6114fe6
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-10-13 10:41:02 +0200

    [INTEGRATION TESTS] tradeService.getAllTrade() => TESTS PASSED

---
+++ src/test/java/com/nnk/springboot/IT/service/TradeServiceGetList_IT.java
@@ -0,0 +1,70 @@
+package com.nnk.springboot.IT.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.nnk.springboot.dto.TradeDTO;
+import com.nnk.springboot.service.TradeService;
+
+@DisplayName("INTEGRATION TESTS - Service ==> Trade Get List")
+@SpringBootTest
+@ActiveProfiles("test")
+class TradeServiceGetList_IT {
+
+	@Autowired
+    private TradeService tradeService;
+
+
+        
+      	// *******************************************************************	
+          
+          @DisplayName("Check Check <NotNull>"
+          		+ " - Given a Trade List,"
+          		+ " when Get Trade List action request,"
+          		+ " then returns Tradeslist not null")    
+          @Test
+          public void testGetTradesListNotNullCheck() throws Exception {
+     	
+             // GIVEN
+
+             // WHEN
+             List<TradeDTO> result = tradeService.getAllTrade();
+             
+             // THEN
+             assertNotNull(result);
+         
+          }
+          
+  
+        // *******************************************************************	
+                      
+
+
+          @DisplayName("Check <Execution Order>"
+          		+ " - Given a Trade List,"
+          		+ " when Get Trade List action request,"
+          		+ " then return expected Number of Trades")    
+          @Test
+          public void testGetTradesListRecordsNumberMatchCheck() throws Exception {
+     	
+             // GIVEN
+
+             // WHEN
+             List<TradeDTO> result = tradeService.getAllTrade();
+             
+             // THEN
+             assertEquals(5, result.size());
+
+          }    
+          
+          // *******************************************************************	
+   
+}
